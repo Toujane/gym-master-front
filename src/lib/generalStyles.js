dd3ef740b0d9fc5689c1592e1011b4cba25c9f10
Mislav Matijević <mislav.mim@gmail.com>
2022-04-22 21:56:57 +0200

    Added styled-components "generalStyles.js" contains styled-components such as buttons and fonts.

---
+++ src/lib/generalStyles.js
@@ -0,0 +1,12 @@
+import { NavLink } from 'react-router-dom';
+import styled, { css, keyframes } from 'styled-components';
+import {
+	borderBlue,
+	borderGrey,
+	boxShadow,
+	boxShadowActive,
+	breakpoints,
+	colors,
+	transitionCubic,
+	transitionEase,
+} from './theme';
