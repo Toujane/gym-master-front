a7261a54d0cf4f9f255266be69b4e7eef51e8da8
Mislav Matijević <mislav.mim@gmail.com>
2022-04-22 19:41:39 +0200

    Added theme.js File consists of 4 breakpoints, colors, borders/shadows/transitions...

---
+++ src/lib/theme.js
@@ -0,0 +1,35 @@
+export const breakpoints = {
+	mobileLarge: 'min-width: 576px',
+	tablet: 'min-width: 768px',
+	desktop: 'min-width: 1024px',
+	desktopLarge: 'min-width: 1300px',
+};
+
+export const colors = {
+	lightBlue: '#57bdeb',
+	mediumBlue: '#1291c9',
+	darkBlue: '#15698f',
+	selectedBlue: '#145a8c',
+	white: '#ffffff',
+	black: '#333333',
+	grey: '#bbbbbb',
+	greyOverlay: 'rgba(10, 10, 10, 0.5)',
+	greyBackground: '#f5f5f5',
+	transparent: 'rgba(0,0,0,0)',
+	error: '#eb5757',
+	success: '#27ae60',
+};
+
+export const boxShadow = '2px 2px 8px 0 rgba(0, 0, 0, 0.2)';
+export const boxShadowActive = '2px 2px 5px rgba(0, 0, 0, 0.6)';
+export const borderBlue = 'solid 2px #15698f';
+export const borderBlueThin = 'solid 1px #15698f';
+export const borderBlueThick = 'solid 3px #15698f';
+export const borderGrey = 'solid 2px #cccccc';
+export const borderGreyThin = 'solid 1px #cccccc';
+export const borderGreyThick = 'solid 3px #cccccc';
+export const borderBlackThin = 'solid 1px #333333';
+export const borderBlack = 'solid 2px #333333';
+export const borderBlackThick = 'solid 3px #333333';
+export const transitionCubic = 'all 0.3s cubic-bezier(.25,.8,.25,1)';
+export const transitionEase = 'all 0.3s ease-out';
