db6ad40b52b4b52022df030977e50380cade6b84
Mislav Matijević <mislav.mim@gmail.com>
2022-04-23 23:21:46 +0200

    Fixed Navigating From Error Page

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Alert } from 'react-bootstrap';
-import { ErrorInner, ErrorMessage, ErrorWrapper } from './ErrorStyle';
+import { useNavigate } from 'react-router-dom';
+import { ErrorInner, ErrorWrapper } from './ErrorStyle';
 
 const Error = ({ message }) => {
+	const navigate = useNavigate();
 	return (
 		<ErrorWrapper>
 			<Alert variant='danger'>
 				<Alert.Heading>Ups!</Alert.Heading>
 				<ErrorInner>{message}</ErrorInner>
 				<br />
-				<Alert.Link href='/'>Vrati se na početnu stranicu.</Alert.Link>
+				<Alert.Link onClick={() => navigate('/')}>Vrati se na početnu stranicu.</Alert.Link>
 			</Alert>
 		</ErrorWrapper>
 	);
